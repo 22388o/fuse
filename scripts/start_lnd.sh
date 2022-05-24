94cfa95875d4e9b3f0cdf0de2de69c8bf95321c4
Mike Dedys <mdedys@users.noreply.github.com>
2022-05-24 17:53:23 -0400

    Remove start_lnd script

--- scripts/start_lnd.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-BTC_HOST=$1 BTC_RPC_PORT=$2 BTC_RPC_USER=$3 BTC_RPC_PASS=$4 BTC_ZQMPUBRAWBLOCK_PORT=$5 BTC_ZQMPUBRAWTX_PORT=$6 docker-compose -f ./docker-compose.lnd.yml up -d
